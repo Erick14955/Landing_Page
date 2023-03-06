6c68d310efe5046935b80eac42ee6d85fe1bbd8c
Erickson Fana Morales <erickson_fana@ucne.edu.do>
2023-03-06 10:10:40 -0400

    All do

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function add7(number){
+    return number + 7;
+}
+
+function multiply(number1, number2){
+    return number1 * number2;
+}
+
+function capitalize(string){
+    string.toUpperCase()[0];
+    return string;
+}
+
+function lastLetter(string){
+    return string[string.length - 1];
+}
